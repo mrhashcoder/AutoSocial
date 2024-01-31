85a1bfeae1443b33bd7eb73d668e02d09fb342f8
Abhishek Sharma <mrhashcoder@Abhisheks-MacBook-Pro.local>
2024-01-31 14:53:32 +0530

    chore : removed few useless thigns

--- config.js
+++ config.js
@@ -3,11 +3,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
-const SUPABASE_URL = process.env.SUPABASE_URL || "https://xaoeqmpiblmgtzqqqhti.supabase.co";
-const SUPABASE_ANONYMOUS_KEY =
-    process.env.SUPABASE_ANONYMOUS_KEY ||
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhhb2VxbXBpYmxtZ3R6cXFxaHRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM0MDk5OTgsImV4cCI6MjAwODk4NTk5OH0.0IlnkTD2DeoxlWHxpy2p8v9eq4nemrzCEpt6YxOz4Vg";
-
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_ANONYMOUS_KEY = process.env.SUPABASE_ANONYMOUS_KEY;
 const STORAGE_BUCKET_NAME = "autosocial";
 
 module.exports = {
