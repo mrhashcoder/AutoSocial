08b7a2f84841e2b8f8937c74b0c93ae7f8fcae3b
Abhishek Sharma <mrhashcoder@Abhisheks-MBP.bbrouter>
2024-01-31 18:36:31 +0530

    another try

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_ANONYMOUS_KEY = process.env.SUPABASE_ANONYMOUS_KEY;
 const STORAGE_BUCKET_NAME = "autosocial";
