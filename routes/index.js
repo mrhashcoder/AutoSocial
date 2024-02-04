afb30c9e41223de5cfe720d279d51e44bd2152a9
Abhishek Sharma <mrhashcoder@Abhisheks-MacBook-Pro.local>
2024-02-04 22:46:22 +0530

    Feat : cors added

--- routes/index.js
+++ routes/index.js
@@ -5,65 +5,76 @@ const path = require("path");
 const createAndUploadMedia = require("../controllers/asyncVideoCreationAndUpload");
 
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-router.post("/create", async (req, res) => {
-    try {
-        const videoGenData = {
-            id: crypto.randomBytes(4).toString("hex"),
-            content: req.body.content,
-            title: req.body.title,
-            tags: req.body.tags,
-        };
+router.post(
+    "/create",
+    [
+        body("title").isString().notEmpty().isLength({ min: 2, max: 50 }).trim(),
+        body("content").isString().notEmpty().isLength({ min: 2 }).trim(),
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            const videoGenData = {
+                id: crypto.randomBytes(4).toString("hex"),
+                content: req.body.content,
+                title: req.body.title,
+                tags: req.body.tags,
+            };
+            const _id = Date.now();
 
-        const _id = Date.now();
+            const videoFileName = `${_id}.mp4`;
+            const signedVideoUrlData = await createSignedUrl(videoFileName, "videos");
 
-        const videoFileName = `${_id}.mp4`;
-        const signedVideoUrlData = await createSignedUrl(videoFileName, "videos");
+            const thumbnailFileName = `${_id}.png`;
+            const signedThumbnailUrlData = await createSignedUrl(thumbnailFileName, "thumbnails");
 
-        const thumbnailFileName = `${_id}.png`;
-        const signedThumbnailUrlData = await createSignedUrl(thumbnailFileName, "thumbnails");
+            if (!signedVideoUrlData === "failed") {
+                return res.status(403).json({
+                    message: "Issue With Database",
+                });
+            }
 
-        if (!signedVideoUrlData === "failed") {
-            return res.status(403).json({
-                message: "Issue With Database",
+            console.log(signedVideoUrlData);
+
+            const videoGenObject = {
+                id: _id,
+                title: videoGenData.title,
+                content: videoGenData.content,
+                tags: videoGenData.tags,
+                videoUrl: createPublicUrl(signedVideoUrlData.path),
+                thumbnailUrl: createPublicUrl(signedThumbnailUrlData.path),
+            };
+
+            const dbObject = await createDbObject(videoGenObject);
+
+            // create a signed URL and submit the object to the db tables
+
+            // const videoPath = await videoGenWorker(videoGenData.content);
+
+            // const videoPath = path.join(__dirname, "..", "public", "output.mp4");
+            // const thumbnailPath = path.join(__dirname, "..", "public", "Frame_00000.png");
+
+            createAndUploadMedia(videoGenObject, signedVideoUrlData, signedThumbnailUrlData);
+
+            return res.status(200).json({
+                message: "Success",
+                data: dbObject,
+            });
+        } catch (e) {
+            console.error(e);
+            return res.status(500).json({
+                status: "failed",
+                message: "EROOR AT SERVER SIDE",
+                data: null,
             });
         }
-
-        console.log(signedVideoUrlData);
-
-        const videoGenObject = {
-            id: _id,
-            title: videoGenData.title,
-            content: videoGenData.content,
-            tags: videoGenData.tags,
-            videoUrl: createPublicUrl(signedVideoUrlData.path),
-            thumbnailUrl: createPublicUrl(signedThumbnailUrlData.path),
-        };
-
-        const dbObject = await createDbObject(videoGenObject);
-
-        // create a signed URL and submit the object to the db tables
-
-        // const videoPath = await videoGenWorker(videoGenData.content);
-
-        // const videoPath = path.join(__dirname, "..", "public", "output.mp4");
-        // const thumbnailPath = path.join(__dirname, "..", "public", "Frame_00000.png");
-
-        createAndUploadMedia(videoGenObject, signedVideoUrlData, signedThumbnailUrlData);
-
-        return res.status(200).json({
-            message: "Success",
-            data: dbObject,
-        });
-    } catch (e) {
-        console.error(e);
-        return res.status(500).json({
-            status: "failed",
-            message: "EROOR AT SERVER SIDE",
-            data: null,
-        });
     }
-});
+);
 
 router.get("/", async (req, res) => {
     return res.status(200).json({ message: "Active" });
